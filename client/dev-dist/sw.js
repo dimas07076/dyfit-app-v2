a6ecc161a4b46cf76abff0f29cf4f712026bd351
DyfitMelo Melo <dyfitmeloadriano@gmail.com>
2025-07-21 16:07:26 +0000

    feat(admin): Mudança

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ck4mvspuulo"
+    "revision": "0.intevo11818"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
